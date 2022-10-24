11246410d3f9a58e6cf090e1d637e9df7357f0f8
Anish Kumar <anishbishnoi127@gmail.com>
2022-10-25 00:31:40 +0530

    navbar (menubar) work done.
    Co-Authored-By: not-just <114912156+not-just@users.noreply.github.com>

--- src/Project.java
+++ src/Project.java
@@ -57,6 +57,89 @@ public class Project extends JFrame {
         student_Leave.setBackground(Color.WHITE);
         leave.add(student_Leave);
 
+        // Leave Details
+        JMenu leave_Details = new JMenu("Leave Details");
+        leave_Details.setForeground(Color.RED);
+        nav.add(leave_Details);
+
+        // nav bar lists items (menu items).
+        JMenuItem faculty_Leave_Details = new JMenuItem("Faculity Leave Details");
+        faculty_Leave_Details.setBackground(Color.WHITE);
+        leave_Details.add(faculty_Leave_Details);
+
+        JMenuItem student_Leave_Details = new JMenuItem("Student Leave Details");
+        student_Leave_Details.setBackground(Color.WHITE);
+        leave_Details.add(student_Leave_Details);
+
+        // Exams
+        JMenu exam = new JMenu("Examination");
+        exam.setForeground(Color.BLUE);
+        nav.add(exam);
+
+        // nav bar lists items (menu items).
+        JMenuItem examination_Details = new JMenuItem("Examination Results");
+        examination_Details.setBackground(Color.WHITE);
+        exam.add(examination_Details);
+
+        JMenuItem enter_marks = new JMenuItem("Enter Marks");
+        enter_marks.setBackground(Color.WHITE);
+        exam.add(enter_marks);
+
+
+        // Update information.
+        JMenu update_Info = new JMenu("Update Details");
+        update_Info.setForeground(Color.RED);
+        nav.add(update_Info);
+
+        // nav bar lists items (menu items).
+        JMenuItem update_Faculty_Info = new JMenuItem("Update Faculty Details");
+        update_Faculty_Info.setBackground(Color.WHITE);
+        update_Info.add(update_Faculty_Info);
+
+        JMenuItem update_Student_Info = new JMenuItem("Update Student Details");
+        update_Student_Info.setBackground(Color.WHITE);
+        update_Info.add(update_Student_Info);
+
+
+        // Fee
+        JMenu fee = new JMenu("Fee Details");
+        fee.setForeground(Color.BLUE);
+        nav.add(fee);
+
+        // nav bar lists items (menu items).
+        JMenuItem fee_Structure = new JMenuItem("Fee Structure");
+        fee_Structure.setBackground(Color.WHITE);
+        fee.add(fee_Structure);
+
+        JMenuItem fee_Form = new JMenuItem("Student Fee Form");
+        fee_Form.setBackground(Color.WHITE);
+        fee.add(fee_Form);
+
+        // Utility
+        JMenu utility = new JMenu("Utility");
+        utility.setForeground(Color.RED);
+        nav.add(utility);
+
+        // nav bar lists items (menu items).
+        JMenuItem notepad = new JMenuItem("Notepad");
+        notepad.setBackground(Color.WHITE);
+        utility.add(notepad);
+
+        JMenuItem calc = new JMenuItem("Calculator");
+        calc.setBackground(Color.WHITE);
+        utility.add(calc);
+
+        // Exit
+
+        JMenu exit = new JMenu("Exit");
+        exit.setForeground(Color.BLUE);
+        nav.add(exit);
+
+        // nav bar lists items (menu items).
+        JMenuItem ex = new JMenuItem("Exit");
+        ex.setBackground(Color.WHITE);
+        exit.add(ex);
+
 
         // nav bar
         setJMenuBar(nav);
